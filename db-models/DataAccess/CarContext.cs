e802e93566b683d5a01b26215bd9030588ea6573
slowee0 <150235819+slowee0@users.noreply.github.com>
2025-02-05 21:46:39 +0100

    proba

---
+++ db-models/DataAccess/CarContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace DataAccess
+{
+    public class CarContext: DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Configurator> Configurators{ get; set; }
+        public DbSet<Extra> Extras{ get; set; }
+        public DbSet<MainPage> MainPage { get; set; }
+        public DbSet<Sale> Sales{ get; set; }
+        public DbSet<User> Users{ get; set; }
+
+        public CarContext() { }
+
+        public CarContext(DbContextOptions<CarContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var conStr = "";
+                optionsBuilder.UseMySql(conStr, ServerVersion.AutoDetect(conStr));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Car>().HasIndex(c => c.Name).IsUnique();
+        }
+
+
+    } 
+}
+ 
---
+++ db-models/Models/MainPage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class MainPage
+    {
+        public required int Id { get; set; }
+        public string? Description { get; set; }
+        [StringLength(250)]
+        public required int? Founded { get; set; }
+        public string? Headquarters { get; set; }
+        [StringLength(30)]
+        public string? Factories { get; set; }
+        [StringLength(30)]
+        public required string? Chairman { get; set; }
+        [StringLength(30)]
+        public required string? Owner { get; set; }
+        [StringLength(30)]
+        public string? Products { get; set; }
+        public int? Employees{ get; set; }
+        
+        public string? History{ get; set; }
+        [StringLength(300)] 
+        public int? Profit { get; set; }
+
+
+
+    }
+}
